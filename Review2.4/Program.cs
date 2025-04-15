83cf8abce5c4a85e2d1165808eed3743558415b8
denzelsilvestre <denzelsilvestre@DENZELSILVE132D>
2025-04-14 20:02:05 -0500

    Add project files.

---
+++ Review2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the hypotenuse Calculator");
+
+Console.WriteLine("Enter the length of the first side (a):");//side a
+
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the length of the first side (b):");// side b
+
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+double c = Math.Sqrt((a * a) * (b * b));//equation
+
+Console.WriteLine("The length of the hypotenuse is:" + c);
+
+Console.WriteLine("Thank you for using the Hypotenuse Calculator!");
